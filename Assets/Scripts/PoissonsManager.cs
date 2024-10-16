ae84534d90fd916f2335f749adcfce3122f3f4ae
Tevadu52 <tevafoug@gmail.com>
2024-10-16 09:35:27 +0200

    [ADD] Poisson control

--- Assets/Scripts/PoissonsManager.cs
+++ Assets/Scripts/PoissonsManager.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "PoissonsManager", order = 0)]
-public class PoissonsManager : ScriptableObject
+public class PoissonsManager : MonoBehaviour
 {
     [SerializeField]
     private Poisson[] Poissons = new Poisson[0];
+    [SerializeField]
+    private GameObject PoissonsHolder;
 
     [SerializeField]
     private Zone[] Zones = new Zone[0];
+    [SerializeField]
+    private int Xmax;
     public static PoissonsManager Instance;
 
     private void Awake()
@@ -22,17 +26,36 @@ public class PoissonsManager : ScriptableObject
     }
     public Poisson GetPoisson(int index) { return Poissons[index]; }
     public Zone GetZone(int index) { return Zones[index]; }
+    public int GetXmax() {return Xmax;}
+
+    public void SpawnPoissons()
+    {
+        foreach (Transform currentPosition in PoissonsHolder.transform)
+        {
+            Destroy(currentPosition.gameObject);
+        }
+        int id = 0;
+        foreach (Poisson poisson in Poissons)
+        {
+            for (int i = 0; i < poisson.NumberByLevel; i++)
+            {
+                GameObject poissonPrefab = Instantiate(poisson.Prefab);
+                poissonPrefab.GetComponent<PoissonController>().Id = i;
+            }
+            id += 1;
+        }
+    }
 }
 
 [System.Serializable]
 public class Poisson
 {
-    [HideInInspector]
-    public int Id;
     public string Name;
     public string Description;
+    public float Speed;
     public int Price;
     public int Level;
+    public int NumberByLevel;
     public GameObject Prefab;
 }
 
