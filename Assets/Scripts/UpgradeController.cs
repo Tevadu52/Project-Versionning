4c4976f96605bd2e80c0798fb9f7356511e248f5
Tevadu52 <tevafoug@gmail.com>
2024-10-18 12:53:49 +0200

    [ADD] Final bugfix

--- Assets/Scripts/UpgradeController.cs
+++ Assets/Scripts/UpgradeController.cs
@@ -40,6 +40,7 @@ public class UpgradeController : MonoBehaviour
         PoissonsStock.Instance.PoissonsClear();
         DisplayMoney();
         amountOfFish = 0;
+        updateButtons();
     }
 
     public void Buy(int price)
